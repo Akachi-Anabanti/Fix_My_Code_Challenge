9d7da6a58471a60c2158ea7cde511f5d79e1c692
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-03-14 23:59:37 +0000

    syntax fix

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 class square():
-    
+
+    """Defines a square class"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -14,15 +14,17 @@ class square():
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """calculates area perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """string method"""
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
