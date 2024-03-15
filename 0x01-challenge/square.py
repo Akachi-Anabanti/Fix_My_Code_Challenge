f2c66ba759f2ebcc21de76f355b496e5a7c3f6f9
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-03-15 00:38:05 +0000

    restored prev value

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -50,7 +50,7 @@ class square:
 
 
 if __name__ == "__main__":
-    s = square(width=9, height=0)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.permiter_of_my_square())
