25085a1a49b7828b706c5e321ca09dbcfc1a8e46
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-03-15 00:35:59 +0000

    update/refactor

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,18 +1,44 @@
 #!/usr/bin/python3
+"""A module that defines a square class"""
 
-class square():
-
+class square:
     """Defines a square class"""
-    width = 0
-    height = 0
+    __width = 0
+    __height = 0
 
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """width setter method"""
+        if type(value) != int:
+            raise TypeError("width is not a valid type")
+        if (value <= 0):
+            raise ValueError("width cannot be <= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """height setter method"""
+        if type(value) != int:
+            raise TypeError("height is not a valid type")
+        if value <= 0:
+            raise ValueError("height cannot be <= 0")
+        self.__height = value
+
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def permiter_of_my_square(self):
         """calculates area perimeter of the square"""
@@ -24,7 +50,7 @@ class square():
 
 
 if __name__ == "__main__":
-    s = square(width=12, height=9)
+    s = square(width=9, height=0)
     print(s)
     print(s.area_of_my_square())
     print(s.permiter_of_my_square())
